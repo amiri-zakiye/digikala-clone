6fc04082fc264a36735b9fd05f8a7fe1395eff65
amiri-zakiye <prsv865@gmail.com>
2025-01-31 23:45:08 +0330

    UPDATE: change develpment port number

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 // const __digicdn_js_challenge = "24127110b24f73fd18b97f7e4e2138ab8669185c";
 
 const nextConfig: NextConfig = {
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
+  // output: "export",  // <=== enables static exports
+  // reactStrictMode: true,
   
   typescript: {
     // !! WARN !!
