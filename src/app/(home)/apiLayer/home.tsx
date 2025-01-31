dcb58379c27d7805dca5231c69620bd7b0b6ca80
amiri-zakiye <prsv865@gmail.com>
2025-01-31 19:39:36 +0330

    fix file path

--- src/app/(home)/apiLayer/home.tsx
+++ src/app/(home)/apiLayer/home.tsx
@@ -3,6 +3,7 @@
 import { apiClient } from "@/lib/apiClient";
 import { ApiResponse } from "../home.types";
 
+export const dynamic = "force-dynamic"
 export const getWidgets = async (): Promise<ApiResponse> => {
   return apiClient.get("widgets/");
 };
