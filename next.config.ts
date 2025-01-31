24b79ca8be792123d28b759c30c25eb1a044d4ad
amiri-zakiye <prsv865@gmail.com>
2025-01-31 19:53:28 +0330

    fix file path

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 // const __digicdn_js_challenge = "24127110b24f73fd18b97f7e4e2138ab8669185c";
 
 const nextConfig: NextConfig = {
-  // output: "export",  // <=== enables static exports
+  output: "export",  // <=== enables static exports
   reactStrictMode: true,
-
+  
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
